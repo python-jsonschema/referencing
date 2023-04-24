7579ff2ce4ef761b11f8a2895e3c3169b40da7fb
Julian Berman <Julian@GrayVines.com>
2023-04-24 17:27:27 -0400

    Fix whatever typing change causes pyright to think frozen classes aren't frozen.

--- referencing/_attrs.pyi
+++ referencing/_attrs.pyi
@@ -8,12 +8,13 @@ _T = TypeVar("_T")
 
 def __dataclass_transform__(
     *,
-    eq_default: bool = ...,
-    order_default: bool = ...,
-    kw_only_default: bool = ...,
+    frozen_default: bool = False,
     field_descriptors: tuple[Union[type, Callable[..., Any]], ...] = ...,
 ) -> Callable[[_T], _T]: ...
 @__dataclass_transform__(field_descriptors=(attrib, field))
 def define(cls: type[_T]) -> type[_T]: ...
-
-frozen = define
+@__dataclass_transform__(
+    frozen_default=True,
+    field_descriptors=(attrib, field),
+)
+def frozen(cls: type[_T]) -> type[_T]: ...
